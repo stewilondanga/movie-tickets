37e0ded8543481f46c224740ef1912a6122ebbb9
Stewart ilondanga <stilondanga@gmail.com>
2017-12-11 17:12:09 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
-$(document).ready(function(){
-  $("#myInput").on("keyup", function(event){
-    var value = $(this).val().toLowerCase();
-    console.log(event.target.value);
-    $("#myLIst li").filter(function(){
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) >-1)
-    })
-  })
-})
+let filterInput = document.getElementById('filter');
+filterInput.addEventListener('keyup', filterNames);
+
+function filterNames() {
+  let filterValue = filterInput.value.toUpperCase();
+  let list = document.getElementById('list');
+  let names = list.querySelectorAll('li.name');
+
+  for (let i = 0; i < names.length; i++) {
+    let nameOutput = names[i].getElementsByTagName('a')[0];
+
+    if(nameOutput.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+      names[i].style.display = '';
+    } else {
+      names[i].style.display = 'none';
+    }
+  }
+}
